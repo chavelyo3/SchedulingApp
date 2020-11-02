7674b4482e774e1a04afb076e9dcc3d39c2a1d24
Chavelyo3 <roxette.banos@gmail.com>
2020-11-01 23:00:36 -0600

    updates

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# SchedulingApp
+<h1>Task:</h1><br>
+this weeks homework was to create a day 9 hour scheduler using local storage and supported by moment.js for time. 
+
+
+
+<br><br><br>
+https://chavelyo3.github.io/SchedulingApp/
+<br>
+<img width="1792" alt="screenshot 1" src="https://user-images.githubusercontent.com/69928551/97539063-60cd0a00-198f-11eb-8289-25c23c578928.png"><br>
+<img width="1774" alt="Screen Shot 2020-10-29 at 2 21 35 AM" src="https://user-images.githubusercontent.com/69928551/97538993-4430d200-198f-11eb-9c3f-f067db6272af.png">
+
+
+<br><br><br>
+<h1>How to use: </h1><br>
+Write in your date to date task and save!
+<br><br><br>
+<h1>Credits:</h1> <br>
+U of M coder
--- script.js
+++ script.js
@@ -1,23 +1,54 @@
+$(document).ready(function () {
+  //hours to be saved to local storage and getting them
+  $("sevenAm").val(localStorage.getItem("7"));
+  $("eightAm").val(localStorage.getItem("8"));
+  $("nineAm").val(localStorage.getItem("9"));
+  $("tenAm").val(localStorage.getItem("10"));
+  $("elevenAm").val(localStorage.getItem("11"));
+  $("twelvePm").val(localStorage.getItem("12"));
+  $("onePm").val(localStorage.getItem("13"));
+  $("twoPm").val(localStorage.getItem("14"));
+  $("threePm").val(localStorage.getItem("15"));
 
-let seven = document.getElementById("7");
-let eight = document.getElementById("8");
-let nine = document.getElementById("9");
-let ten = document.getElementById("10");
-let eleven = document.getElementById("11");
-let twelve = document.getElementById("12");
-let one = document.getElementById("1");
-let two = document.getElementById("2");
-let three = document.getElementById("3");
+  // save button local storage
+  $(".saveBtn").on("click", function () {
+    let task = $(this).siblings(".describtion").val();
+    console.log($(this));
+    let time = $(this).parent().attr("id");
+    localStorage.setItem(time, task);
+    console.log(time, task);
+  });
 
-let m = moment();
+  let m = moment();
+  // show current date and time on header
+  //current date
+  let currentDateEl = document.getElementById("currentDate");
+  currentDateEl.innerHTML = m.format("LL");
+  // current time
+  let currentTimeEl = document.getElementById("currentTiempo");
+  currentTimeEl.innerHTML = m.format("LT");
 
-//show current date
-let currentDateEl = document.getElementById("currentDate");
-currentDateEl.innerHTML = m.format("LL");
+  //
+  function dayTracker() {
+    let currentHour = moment().hours();
+    console.log(currentTiempo);
+    $(".time-block").each(function () {
+      console.log($(this));
+      let timeHour = parseInt($(this).attr("id").split("-")[1]);
 
-// show current time 
-let currentTimeEl = document.getElementById("currentTime");
-currentTimeEl.innerHTML = m.format("LT");
+      if (timeHour < currentTiempo) {
+        $(this).addClass("past");
+      } else if (timeHour === currentTiempo) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      }
+      if (timeHour > currentTiempo) {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
 
-
-
+  dayTracker();
+});
